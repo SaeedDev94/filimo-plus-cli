d2b1aae0f7a7f0abe7e711370236a49d59b900b2
SaeedPooyanfar <saeedp47@gmail.com>
2021-12-04 00:24:16 +0330

    modify dl.bash

--- dl.bash
+++ dl.bash
@@ -9,7 +9,7 @@ videoFile="$downloadPath/$id.mp4"
 videoStream="$3"
 audioStream="$4"
 
-if [ -n $audioStream ]
+if [ -n "$audioStream" ]
 then
   ffmpeg -i "$videoStream" -i "$audioStream" -map 0:v -map 1:a -codec copy -shortest -y "$videoFile" </dev/null >/dev/null 2> "$logFile" &
 else
