b26dff92ea0368627e9e9e8da02616b7fb020532
SaeedPooyanfar <saeedp47@gmail.com>
2021-12-08 18:24:29 +0330

    AuthService#getUserAgent: return value by platform

--- src/Auth/AuthService.ts
+++ src/Auth/AuthService.ts
@@ -23,7 +23,14 @@ export class AuthService {
   }
 
   getUserAgent(): string {
-    return 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36';
+    switch (process.platform) {
+      case 'linux':
+        return 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36';
+      case 'win32':
+        return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36';
+      default:
+        return 'filimo-plus-cli';
+    }
   }
 
 }
