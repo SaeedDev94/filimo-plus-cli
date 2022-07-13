7de671dcbf910daf09cc966673bb60ac3cdb87e5
Hamed H <h1376h@gmail.com>
2022-07-13 11:36:02 +0430

    Update dl.bash

--- dl.bash
+++ dl.bash
@@ -7,8 +7,12 @@ logFile="$3"
 
 videoStream="$4"
 audioStream="$5"
+audioStream2="$6"
 
-if [ -n "$audioStream" ]
+if [ -n "$audioStream2" ]
+then
+  ffmpeg -headers "$headers" -i "$videoStream" -i "$audioStream" -i "$audioStream2" -map 0:v -map 1:a -map 2:a -codec copy -shortest -y "$itemFile" </dev/null >/dev/null 2> "$logFile" &
+elif [ -n "$audioStream" ]
 then
   ffmpeg -headers "$headers" -i "$videoStream" -i "$audioStream" -map 0:v -map 1:a -codec copy -shortest -y "$itemFile" </dev/null >/dev/null 2> "$logFile" &
 else
